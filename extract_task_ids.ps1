28e3cc02506047b317ba4c1e11747ba05270596b
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-17 15:42:11 +1100

    unpacking tests

--- extract_task_ids.ps1
+++ extract_task_ids.ps1
@@ -1,23 +1,12 @@
 # Load the XML file
 [xml]$xmlContent = Get-Content 'project.xml'
 
-# Initialize an array to hold the output
-$output = @()
-
-# Loop through each 'Element' node in the XML
-foreach ($element in $xmlContent.Element.Element) {
-    # Check if the 'Type' is 'TASK'
-    if ($element.Type -eq 'TASK') {
-        # Create a custom object with 'label' and 'ID' properties
-        $obj = New-Object PSObject -Property @{
-            'Label' = $element.label
-            'ID'    = $element.ID
-        }
-        
-        # Add the object to the output array
-        $output += $obj
+$tasks = $xmlContent | Select-Xml -XPath '//Element[@Type="SAS.EG.ProjectElements.CodeTask"]/Element' | ForEach-Object {
+    [PSCustomObject]@{
+        'Type' = $_.Node.Type
+        'ID' = $_.Node.ID
+        'Label' = $_.Node.label
     }
 }
-
 # Export the output array to a CSV file
-$output | Export-Csv -Path 'code_task_map.csv' -NoTypeInformation
+$tasks | Export-Csv -Path 'code_task_map.csv' -NoTypeInformation
