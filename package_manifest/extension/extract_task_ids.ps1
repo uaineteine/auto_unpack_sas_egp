06c31a6deefff2ff2bca555531e606cc9e35d841
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-23 15:39:01 +1100

    moved to packing dir

---
+++ package_manifest/extension/extract_task_ids.ps1
@@ -0,0 +1,15 @@
+# Load the XML file
+[xml]$xmlContent = Get-Content 'unpacked/project.xml'
+
+$tasks = $xmlContent | Select-Xml -XPath '//Element[@Type="SAS.EG.ProjectElements.CodeTask"]/Element' | ForEach-Object {
+    [PSCustomObject]@{
+        'Type' = $_.Node.Type
+        'ID' = $_.Node.ID
+        'Label' = $_.Node.label
+    }
+}
+# Export the output array to a CSV file
+if (!(Test-Path -Path "processed")) {
+    New-Item -ItemType Directory -Path "processed" | Out-Null
+}
+$tasks | Export-Csv -Path 'processed/code_task_map.csv' -NoTypeInformation
---
+++ package_manifest/extension/sort_code_tasks.ps1
@@ -0,0 +1,23 @@
+# Import the CSV file
+$csvData = Import-Csv -Path "processed/code_task_map.csv"
+
+# Iterate over each row in the CSV
+foreach ($row in $csvData) {
+    # Define the source and destination paths
+    $sourcePath = "unpacked/$($row.ID)/code.sas"
+    $destinationPath = "processed/$($row.Label).sas"
+
+    # Check if the source file exists
+    if (Test-Path -Path $sourcePath) {
+        # Create the destination directory if it doesn't exist
+        $destinationDir = Split-Path -Path $destinationPath -Parent
+        if (!(Test-Path -Path $destinationDir)) {
+            New-Item -ItemType Directory -Path $destinationDir | Out-Null
+        }
+
+        # Copy the file
+        Copy-Item -Path $sourcePath -Destination $destinationPath
+    } else {
+        Write-Host "Source file $sourcePath does not exist."
+    }
+}
